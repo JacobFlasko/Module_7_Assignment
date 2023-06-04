967720b3a3d6b3539cb47e7406eccdd2bae37deb
Jacob Flasko <115510621+JacobFlasko@users.noreply.github.com>
2023-06-04 13:54:02 -0400

    Create MapsActivity.kt

---
+++ MapsActivity.kt
@@ -0,0 +1,50 @@
+package com.example.module_7_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.module_7_assignment.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun saveLocation(latLng: LatLng) =
+            getPreferences(MODE_PRIVATE)?.edit()?.apply {
+                putString("latitude", latLng.latitude.toString())
+                putString("longitude", latLng.longitude.toString())
+                apply()
+            }Copy
+                    Restore any saved location from SharedPreferences. You can use the following function:
+    val latitude = sharedPreferences.getString("latitude", null)      ?.toDoubleOrNull() ?: return null
+    val longitude = sharedPreferences.getString("longitude",       null)?.toDoubleOrNull()       ?: return null
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
